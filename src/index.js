084a6ce155d325b7087e1ef67fa1762d870923c7
Michael Stark <michael@goStark.com>
2024-02-18 16:46:50 -0500

    fixed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './css/bootstrap/css/bootstrap.min.css';
-import './css/bootstrap/css/bootstrap-theme.min.css';
 import './css/open-iconic/font/css/open-iconic-bootstrap.css';
 import './css/paper-kit.css';
 import './css/demo.css';
 import './css/site.css';
-import './css/variables.css';
 
 import './index.css';
 import App from './App';
