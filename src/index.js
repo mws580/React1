c0a4afb9d80599e08008f8032bd26a1db46f3fe6
Michael Stark <michael@goStark.com>
2024-02-18 16:58:16 -0500

    fixed css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './css/bootstrap/bootstrap.min.css';
-import './css/open-iconic/font/css/open-iconic-bootstrap.css';
+import './css/open-iconic/font/css/open-iconic-bootstrap.min.css';
 import './css/paper-kit.css';
 import './css/demo.css';
 import './css/site.css';
