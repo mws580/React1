278f3d605967169f4f1508b2f4cf684f0dbc1566
Michael Stark <michael@goStark.com>
2024-11-01 14:03:54 -0400

    a change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = ' There is more';
-  const value2 = 'And I am beside myself!';
+  const value2 = 'And I am beside myself!!';
   // #return <div>Hello {value}</div>;
   return <div> <h1 class="h"> Yes! </h1><h2 class=" text-center">{value} </h2><h3 class=" text-center">{value2} </h3></div>;
 
